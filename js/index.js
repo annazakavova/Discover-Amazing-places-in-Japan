9e28d21dd244827e626efdb548cb128ff6ba388e
Anna <zakavova94@icloud.com>
2022-03-17 16:56:08 +0300

    finished project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// HEADER
 (function () {
 	const header =document.querySelector('.header');
 	window.onscroll = () => {
@@ -9,6 +10,7 @@
 	};
 }());
 
+// BURGER
 (function () {
 	const burgerItem = document.querySelector('.burger');
 	const menu = document.querySelector('.header__nav');
@@ -20,4 +22,44 @@
 	menuCloseItem.addEventListener('click', () => {
 		menu.classList.remove('header__nav_active');
 	});
+}());
+
+// SCROLL
+(function () {
+
+	const smoothScroll = function (targetEl, duration) {
+		 const headerElHeight =  document.querySelector('.header').clientHeight;
+		 let target = document.querySelector(targetEl);
+		 let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+		 let startPosition = window.pageYOffset;
+		 let startTime = null;
+	
+		 const ease = function(t,b,c,d) {
+			  t /= d / 2;
+			  if (t < 1) return c / 2 * t * t + b;
+			  t--;
+			  return -c / 2 * (t * (t - 2) - 1) + b;
+		 };
+	
+		 const animation = function(currentTime){
+			  if (startTime === null) startTime = currentTime;
+			  const timeElapsed = currentTime - startTime;
+			  const run = ease(timeElapsed, startPosition, targetPosition, duration);
+			  window.scrollTo(0,run);
+			  if (timeElapsed < duration) requestAnimationFrame(animation);
+		 };
+		 requestAnimationFrame(animation);
+
+	};
+
+	const scrollTo = function () {
+		 const links = document.querySelectorAll('.js-scroll');
+		 links.forEach(each => {
+			  each.addEventListener('click', function () {
+					const currentTarget = this.getAttribute('href');
+					smoothScroll(currentTarget, 1000);
+			  });
+		 });
+	};
+	scrollTo();
 }());
